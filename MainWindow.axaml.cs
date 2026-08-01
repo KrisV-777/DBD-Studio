d5e9483d148c0d8a527275d817d370ef6f8146d4
Kris <kris.vlntn@gmail.com>
2026-08-01 14:27:42 +0200

    init

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Body_Distribution_Studio;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
