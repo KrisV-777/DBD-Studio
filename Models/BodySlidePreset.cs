e0ab72d73c7c775bd5f4fe4269e8042057c00db1
Kris <kris.vlntn@gmail.com>
2026-08-11 18:05:59 +0200

    remove unused models

--- Models/BodySlidePreset.cs
+++
@@ -1,8 +0,0 @@
-namespace DBDStudio.Models
-{
-    public sealed class BodySlidePreset
-    {
-        public string Preset { get; init; } = string.Empty;
-        public string XmlFile { get; init; } = string.Empty;
-    }
-}
