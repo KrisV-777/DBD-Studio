a9aac6fff7037c5f1e6944eaa70b879b8238d440
Kris <kris.vlntn@gmail.com>
2026-09-18 21:42:27 +0200

    fix FormReference syntax

--- DBDStudio/Models/Mutagen/FormReference.cs
+++ DBDStudio/Models/Mutagen/FormReference.cs
@@ -2,7 +2,7 @@ namespace DBDStudio.Models.Mutagen
 {
     public sealed record FormReference(string Plugin, uint FormId)
     {
-        public override string ToString() => $"{Plugin}|0x{FormId:X6}";
+        public override string ToString() => $"0x{FormId:X6}|{Plugin}";
 
         public bool MaybeValid() => !string.IsNullOrWhiteSpace(Plugin) && FormId != 0;
     }
